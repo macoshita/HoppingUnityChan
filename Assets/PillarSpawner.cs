3c8e7377be31fc4c09e45ca26668f4a6de497388
macoshita <macoshita@gmail.com>
2015-03-31 00:41:17 +0900

    Random Spawn Pillar

--- Assets/PillarSpawner.cs
+++ Assets/PillarSpawner.cs
@@ -18,10 +18,10 @@ public class PillarSpawner : MonoBehaviour {
 
     void Spawn()
     {
-        //float y = Random.Range(0f, 5f);
-		float y = 0f;
+        float y = Random.Range(0f, 5f);
+		//float y = 0f;
 		Instantiate(pillar, new Vector3(0, y, z), Quaternion.identity);
-    //    z += Random.Range(3f, 8f);
-		z += 1f;
+    	z += Random.Range(3f, 8f);
+		//z += 1f;
 	}
 }

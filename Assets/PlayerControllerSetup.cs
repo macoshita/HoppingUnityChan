4672fe1f911e9c38c63be308e562f06db5c5d73c
macoshita <macoshita@users.noreply.github.com>
2015-04-01 02:06:08 +0900

    Resolved the complex code

--- Assets/PlayerControllerSetup.cs
+++ Assets/PlayerControllerSetup.cs
@@ -16,78 +16,65 @@ public class PlayerControllerSetup : MonoBehaviour {
     private Animator animator;
 
 	JumpState jumpState = JumpState.Grounded;
-	enum JumpState { Grounded, WillJump, Jumping };
+	enum JumpState {
+		Grounded = 0,
+		WillJump = 1,
+		Jumping = 2,
+		Falling = 3
+	};
 
     // Use this for initialization
     void Start () {
         character = GetComponent<CharacterController> ();
-        animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>(); 
     }
     
     // Update is called once per frame
     void Update () {
-		if (jumpState == JumpState.Grounded) {
-			// 着地状態ならジャンプできる
-			if (Input.GetKeyDown (KeyCode.Space)) {
-				// スペースキーを押したらジャンプの準備
-				jumpState = JumpState.WillJump;
-			}
-		} else if (jumpState == JumpState.WillJump) {
-			if (Input.GetKey (KeyCode.Space)) {
-				// スペースキーを押し続けてジャンプ力を溜める
-				jumpSpeed += 20F * Time.deltaTime;
-				if (jumpSpeed > 15f) {
-					// 一定量以上は溜まらない
-					jumpSpeed = 15f;
+		if (character.isGrounded) {
+			velocity = new Vector3(0, 0, 0);
+
+			if (jumpState == JumpState.Grounded) {
+				// 着地状態ならジャンプできる
+				if (Input.GetKeyDown (KeyCode.Space)) {
+					// スペースキーを押したらジャンプの準備
+					jumpState = JumpState.WillJump;
+					animator.speed = 0.25f;
 				}
-				
-			} else {
-				// スペースキーを離したらジャンプ
-				animator.speed = 1f;
-			}
-		} else if (jumpState == JumpState.Jumping) {
-			if (character.isGrounded) {
-				velocity = new Vector3(0, 0, 0);
+			} else if (jumpState == JumpState.WillJump) {
+				if (Input.GetKey (KeyCode.Space)) {
+					// スペースキーを押し続けてジャンプ力を溜める
+					jumpSpeed += 20F * Time.deltaTime;
+					if (jumpSpeed > 15f) {
+						// 一定量以上は溜まらない
+						jumpSpeed = 15f;
+					}
+					
+				} else {
+					// スペースキーを離したらジャンプ
+					velocity = new Vector3(0, jumpSpeed + 5F, 4F);
+					jumpSpeed = 0F;
+					jumpState = JumpState.Jumping;
+					animator.speed = 1f;
+				}
+			} else if (jumpState == JumpState.Falling) {
 				jumpState = JumpState.Grounded;
+			}
+
+		} else {
+			if (velocity.y < 0) {
+				jumpState = JumpState.Falling;
 			}
 		}
 
-		if (jumpState == JumpState.Grounded) {
-			animator.speed = 1f;
-			animator.SetBool ("Falling", false);
-		} else if (jumpState == JumpState.WillJump) {
-			animator.SetBool ("WillJump", true);	
-		} else if (jumpState == JumpState.Jumping) {
-			animator.SetBool ("WillJump", false);
-			if (velocity.y < 0) {
-				animator.SetBool ("Falling", true);
-			}
-		}
+		Debug.Log((int)jumpState);
+
+  		animator.SetInteger("JumpState", (int)jumpState);
 
         velocity.y -= gravity * Time.deltaTime;        // 移動量に重力を加える
 
         CollisionFlags flag = character.Move (velocity * Time.deltaTime);
     }
-
-	void OnJumpStart()
-	{
-		animator.speed = 0.25f;
-	}
-
-	void OnMaxJump()
-	{
-		if (jumpState == JumpState.WillJump) {
-			animator.speed = 0;
-		}
-	}
-
-    void OnLeaveFeet()
-    {
-		velocity = new Vector3(0, jumpSpeed + 5F, 4F);
-		jumpSpeed = 0F;
-		CollisionFlags flag = character.Move (velocity * Time.deltaTime);
-		jumpState = JumpState.Jumping;
-	}
 
 	// アニメーション的に完全に落ちはじめた時に呼び出される
 	void OnFalling()
